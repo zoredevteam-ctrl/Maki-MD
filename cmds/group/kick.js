95fd92870b889899b43d7544b836841523fe9a98
Carlos <supernumerovirtual@gmail.com>
2026-08-23 20:37:23 -0400

    Create kick.js

---
+++ cmds/group/kick.js
@@ -0,0 +1,21 @@
+const handler = async (m, { conn, who }) => {
+    if (!who) return m.reply('⚠️ *Menciona al usuario que desea expulsar*')
+
+    const botJid = conn.user.id.split(':')[0] + '@s.whatsapp.net'
+
+    if (who === botJid) return m.reply('⚠️ No puedo expulsarme a mí mismo')
+
+    try {
+        await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
+        m.reply('✅ Usuario eliminado')
+    } catch {
+        m.reply('❌ No quiero chambear')
+    }
+}
+
+handler.command = ['kick', 'echar', 'hechar', 'sacar', 'ban']
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler
