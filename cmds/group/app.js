81214c1f683a06774746d0a09220556ba313a3e3
Carlos <supernumerovirtual@gmail.com>
2026-08-25 16:29:58 -0400

    Actualizar app.js

--- cmds/group/app.js
+++ cmds/group/app.js
@@ -1,13 +1,16 @@
-const handler = async (m, { conn, command }) => {
+const handler = async (m, { conn, args }) => {
+  const op = (args[0] || '').toLowerCase()
+  if (!['on', 'off'].includes(op)) return m.reply('⚠️ Debes especificar si deseas *activar* o *desactivar* esta cosa')
+
   try {
-    await conn.groupSettingUpdate(m.chat, command === 'app on' ? 'approval' : 'not_approval')
-    m.reply(`✅ Aprobación ${command === 'app on' ? 'activada' : 'desactivada'}`)
+    await conn.groupSettingUpdate(m.chat, op === 'on' ? 'approval' : 'not_approval')
+    m.reply(`✅ Aprobación de admin para unirse ${op === 'on' ? 'activada' : 'desactivada'}`)
   } catch {
     m.reply('❌ Error al cambiar la configuración')
   }
 }
 
-handler.command = ['app on', 'app off']
+handler.command = ['app']
 handler.tags = ['group']
 handler.admin = true
 handler.group = true
